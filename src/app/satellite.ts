92d1b59752f6869eb32f6ff2a583cb49aed4e113
Kaitlyn forks <kaitlynforks@VT-100541.local>
2020-08-04 20:42:53 -0500

    finished assignment

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,8 +5,21 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
-    constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-        
+    constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
+
+    shouldShowWarning(): boolean {
+        if (this.type == "Space Debris") {
+         return true
+        }
+
+        else {return false}
+
     }
 }
   
